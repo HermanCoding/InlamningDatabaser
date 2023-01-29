c621aba9434cd02e2703f59a1efe7e21178da591
HermaN <d.herman85@gmail.com>
2023-01-29 19:53:35 +0100

    final

---
+++ InlamningDatabaser/DBLogin.cs
@@ -0,0 +1,18 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InlamningDatabaser
+{
+    internal class DBLogin
+    {
+        static string server = "localhost";
+        static string database = "paintings";
+        static string user = "root";
+        static string pass = "";
+        public string connString = $"SERVER={server};DATABASE={database};UID={user};PASSWORD={pass};";
+    }
+}
